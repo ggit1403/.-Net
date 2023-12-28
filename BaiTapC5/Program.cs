b7b65bae061122764a9be7c594ebaeb42f12284e
ggit14103 <giang2k3qtkx@gmail.com>
2023-12-28 07:05:40 +0700

    up bai 5

--- BaiTapC5/Program.cs
+++ BaiTapC5/Program.cs
@@ -1,69 +1,177 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-class Person
-{
+// bai1
+class Person{
     public int Age;
     public string name;
     public void Hello() {
         Console.Write("Name: ");
         name = Console.ReadLine();
-        Console.WriteLine("Hello, i'm "+name);
+        Console.WriteLine("Hello, i'm " + name);
     }
     public void SetAge()
     {
-        Console.Write("Dat tuoi: ");
+        Console.Write("Nhap tuoi cua ban: ");
         Age = int.Parse(Console.ReadLine());
-    }
-    static void Bai1()
-    {
-       //Person p1 = new Person(); khởi tạo thông qua toán tử New
-        //p1.Hello();
-        //p1.SetAge(20);
-        Student student = new Student();
-        student.Hello();
-        student.Study();
-        student.SetAge();
-        student.ShowAge();
-        
-        Teacher teacher = new Teacher();
-        teacher.Teach();    
-    }
-    static void Main(string[] args)
-    {
-        Bai1();
-    }
+    }  
 }
-
-/*
-    class Student : Person
-{
-    public void Study()
-    {
-        Console.WriteLine("T'm studying");
-    }
-    public void ShowAge()
-    {
-        Console.Write("Dat tuoi: ");
-        Age = int.Parse(Console.ReadLine());
-        Console.WriteLine("My age is: " + Age + "year old");
-    }
-}
-*/
-
-/*
-   class Teacher : Person
+class Teacher : Person
 {
     public void Teach()
     {
+        Console.WriteLine("Hello!");
         Console.WriteLine("I'm Teaching");
     }
-    public void AgeTeach()
+}
+class Student : Person
+{
+    public void Study()
     {
-        Console.Write("Dat tuoi: ");
-        Age = int.Parse(Console.ReadLine());
-        Console.WriteLine("I'm " + Age + " years old");
+        Console.WriteLine("I'm studying");
+    }
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is: " + Age + " year old");
     }
 }
-*/
+//bai2
+class Hinh_Hoc
+{
+    public double chieu_dai, width, r;
+    public double pi = 3.14;
+}
+class HCN : Hinh_Hoc 
+{
+    
+    public void HinhChuNhat()
+    {
+        Console.WriteLine("Hinh chu nhat, nhap chieu dai va chieu rong:");
+        chieu_dai = double.Parse(Console.ReadLine());
+        width = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Chu vi: ");
+        Chu_vi();
+        Console.WriteLine("Dien tich: ");
+        Dien_tich();
+    }
+    
+    public void Chu_vi()
+    {
+        double chuvi = (chieu_dai + width) * 2;
+        Console.WriteLine(chuvi);
+    }
+    public void Dien_tich()
+    {
+        double dien_tich = chieu_dai * width;
+        Console.WriteLine(dien_tich);
+    }
+}
+class Circle : Hinh_Hoc
+{
+
+    public void HinhTron()
+    {
+        Console.WriteLine("Hinh tron, nhap ban kinh:");
+        r = double.Parse(Console.ReadLine());
+        Console.WriteLine("Chu vi: ");
+        Chu_vi();
+        Console.WriteLine("Dien tich: ");
+        Dien_tich();
+    }
+
+    public void Chu_vi()
+    {
+        double chuvi = pi*r*2;
+        Console.WriteLine(chuvi);
+    }
+    public void Dien_tich()
+    {
+        double dien_tich = pi*r*r;
+        Console.WriteLine(dien_tich);
+    }
+
+}
+//bai3
+class TKNH
+{
+    private string stk;
+    private double sodu;
+    public  void  Nhapstk() {
+        Console.Write("Nhap so tai khoan: ");
+        stk = Console.ReadLine();
+        Console.WriteLine("So tai khoan :"+stk);
+        sodu = 500.000;
+        Console.WriteLine("So du tai khoan: "+sodu);
+    }
+    public void GuiTien() {
+        Console.Write("Nhap so tien muon gui: ");
+        double tien_gui = double.Parse(Console.ReadLine());
+        if(tien_gui > 0) {
+            sodu += tien_gui;
+            Console.WriteLine("So du hien tai: " + sodu);
+        }
+        else { Console.WriteLine("Nhap sai!"); }
+    }
+    public void RutTien()
+    {
+        Console.Write("Nhap so tien muon rut: ");
+        double tien_rut = double.Parse(Console.ReadLine());
+        if (tien_rut > 0 && tien_rut <= sodu)
+        {
+            sodu -= tien_rut;
+            Console.WriteLine("So du hien tai: " + sodu);
+        }
+        else if(tien_rut == 0)
+        {
+            Console.WriteLine("Vui long nhap so tien rut lon hon 0");
+        }
+        else { Console.WriteLine("Nhap sai!"); }
+    }
+}
 
 
+class Program
+{
+    static void Bai1()
+    {
+        Console.WriteLine("Bai1:");
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+
+        student.Study();
+        student.SetAge();
+        student.ShowAge();
+
+        teacher.Teach();
+    }
+    static void Bai2()
+    {
+        Console.WriteLine("Bai2:");
+        HCN hcn = new HCN();
+        hcn.HinhChuNhat();
+        Circle circle = new Circle();
+        circle.HinhTron();
+    }
+    static void Bai3()
+    {
+        Console.WriteLine("Bai3:");
+        TKNH taikhoan = new TKNH();
+        taikhoan.Nhapstk();
+        taikhoan.GuiTien();
+        taikhoan.RutTien();
+    }
+    static void Bai4()
+    {
+
+    }
+
+    static void Main(string[] args)
+    {
+        //Bai1();
+        //Bai2();
+        //Bai3();
+
+    }
+}
+    
--- BaiTapC5/Teacher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class Teacher : Person
-{
-    public void Teach()
-    {
-        Console.WriteLine("I'm Teaching");
-    }
-}
